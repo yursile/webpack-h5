10032635db8755fbb635c5fe4d36a0ac2fbec1c4
yursile <289181450@qq.com>
2016-09-05 10:50:55 +0800

    rem

--- online.config.js
+++ online.config.js
@@ -29,8 +29,8 @@ module.exports={
          loaders: [ //加载器
             {
                 test: /\.less$/,
-                // loader: 'style!css!px2rem?remUnit=75&remPrecision=8!postcss!less'
-               loader: ExtractTextPlugin.extract("style", "css-loader?-minimize!px2rem?remUnit=100&remPrecision=8!postcss!less")
+                // loader: ExtractTextPlugin.extract("style", "css-loader?-minimize!postcss!less")//不用rem
+                loader: ExtractTextPlugin.extract("style", "css-loader?-minimize!px2rem?remUnit=100&remPrecision=8!postcss!less")
             },
             {
                 test: /\.js[x]?$/,
@@ -40,7 +40,7 @@ module.exports={
             {
                 test: /\.css$/, 
                 loader:ExtractTextPlugin.extract("style", "css-loader?-minimize!px2rem?remUnit=100&remPrecision=8") 
-                // loader:ExtractTextPlugin.extract("style", "css-loader?-minimize","px2rem-loader?remUnit=75&remPrecision=8","postcss") 
+                // loader:ExtractTextPlugin.extract("style", "css-loader?-minimize")//不用rem
             },
             {
                 test: /\.html$/, 
